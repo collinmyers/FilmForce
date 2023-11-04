c87f2ebc93ba1dac768ba45fce7325d6c44885b3
Collin Myers <collin_myers@outlook.com>
2023-11-03 22:37:53 -0400

    Got search page to show. Needs styled and search needs implemented

--- src/pages/Main/Search.jsx
+++ src/pages/Main/Search.jsx
@@ -6,36 +6,77 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Search = () => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log('User is signed in:', user);
+                setLoggedIn(true);
+            } else {
+                console.log('No user is signed in.');
+                setLoggedIn(false);
+            }
+            setIsLoading(false);
+        }, (error) => {
+            console.error('Auth state change error:', error);
+            setIsLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    if (isLoading) {
+        return;
+    }
+
+    const handleLogout = () => {
+        signOut(auth)
+            .then(() => {
+                setLoggedIn(false);
+                auth.currentUser.reload();
+                navigate("/")
+            })
+            .catch((error) => {
+                console.error('Logout Error:', error);
+            });
+    };
+
+
     return (
         <div>
-          <header className="site-header sh-home">
-            <h1>Welcome to Film<span id="home-force">Force</span></h1>
-            <p>For the Love of Cinema</p>
-          </header>
-      
-          <nav className="main-nav">
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/Search">Search</a></li>
-              {loggedIn && <li><a href="/Settings">Settings</a></li>}
-              {loggedIn ? (
-                <li><a onClick={handleLogout} id='logout'>Logout</a></li>
-              ) : (
-                <li><a href="/Login">Login</a></li>
-              )}
-            </ul>
-          </nav>
-      
-          <main className="content" id="search-section">
-            <div className="search-bar">
-              <input type="text" placeholder="Search for movies" />
-              <button onClick={handleSearch}>Search</button>
-            </div>
-            
-    
-          </main>
+            <header className="site-header sh-home">
+                <h1>Welcome to Film<span id="home-force">Force</span></h1>
+                <p>For the Love of Cinema</p>
+            </header>
+
+            <nav className="main-nav">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/Search">Search</a></li>
+                    {loggedIn && <li><a href="/Settings">Settings</a></li>}
+                    {loggedIn ? (
+                        <li><a onClick={handleLogout} id='logout'>Logout</a></li>
+                    ) : (
+                        <li><a href="/Login">Login</a></li>
+                    )}
+                </ul>
+            </nav>
+
+            <main className="content" id="search-section">
+                <div className="search-bar">
+                    <input type="text" placeholder="Search for movies" />
+                    <button>Search</button>
+                    {/* <button onClick={handleSearch}>Search</button> */}
+                </div>
+
+
+            </main>
         </div>
-      );
+    );
 
 };
 
